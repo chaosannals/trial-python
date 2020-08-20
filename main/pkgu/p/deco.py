f1d7a4997804ecdaf2fe8c3cddb101c92537bf60
chaosannals <chaosannals@outlook.com>
2020-08-20 10:17:39 +0800

    wrap

--- main/pkgu/p/deco.py
+++ main/pkgu/p/deco.py
@@ -1,3 +1,8 @@
+import functools
+
 def d_func(func):
-    print(func.__name__)
-    return func
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print(func.__name__)
+        func(*args, **kwargs)
+    return wrapper
