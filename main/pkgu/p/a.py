f8223de8be93de17aaabb16e35e28e4fb22887a9
chenshenchao <chenshenchao@outlook.com>
2020-08-22 01:16:36 +0800

    cls

--- main/pkgu/p/a.py
+++ main/pkgu/p/a.py
@@ -1,7 +1,22 @@
 def d_1(method):
-    print(dir(method))
-    print(method.__qualname__)
+    # for k in dir(method):
+    #     v = getattr(method, k)
+    #     print(k)
+    #     print(v)
     return method
+
+class ddd:
+    def __init__(self, method):
+        self.method = method
+    
+    def __get__(self, instance, cls):
+        print('ddd def')
+        print(cls)
+        def wrapper(*args, **kwargs):
+            print('dd call')
+            return self.method(instance, *args, **kwargs)
+        print(wrapper.__name__)
+        return wrapper
 
 class A:
     '''
@@ -11,6 +26,7 @@ class A:
         '''
         '''
 
+    @ddd
     @d_1
     def m1(self):
         print('m1')
