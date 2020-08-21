9f1b2f3ba4256841aad5a8e7bd28af8d25d0b49e
chenshenchao <chenshenchao@outlook.com>
2020-08-22 00:55:10 +0800

    cls

--- main/pkgu/p/a.py
+++ main/pkgu/p/a.py
@@ -1,1 +1,16 @@
-print('a')
+def d_1(method):
+    print(dir(method))
+    print(method.__qualname__)
+    return method
+
+class A:
+    '''
+    '''
+
+    def __init__(self):
+        '''
+        '''
+
+    @d_1
+    def m1(self):
+        print('m1')
