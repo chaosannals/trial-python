93a01020df77cfcf2768ae1f056101d532137587
chaosannals <chaosannals@outlook.com>
2020-10-26 21:46:24 +0800

    subproc

---
+++ main/aio/subproc.py
@@ -0,0 +1,22 @@
+import asyncio
+
+async def run(cmd):
+    '''
+    '''
+
+    process = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    stdout, stderr = await process.communicate()
+
+    print(f'[{cmd!r} exited with {process.returncode}]')
+    if stdout:
+        print(f'[stdout]\n{stdout.decode("gbk")}')
+        pass
+    if stderr:
+        print(f'[stderr]\n{stderr.decode("gbk")}')
+        pass
+
+asyncio.run(run('ls'))
