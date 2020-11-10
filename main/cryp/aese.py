be8b9573bdf211b3ac72bbbbc28c0e000b981340
chaosannals <chaosannals@outlook.com>
2020-11-10 17:12:09 +0800

    加密。

---
+++ main/cryp/aese.py
@@ -0,0 +1,11 @@
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
+data = b'aaaaaaabbbbbbb'
+key = b'1234567890qwerty'
+cipher = AES.new(key, AES.MODE_EAX)
+ciphertext, tag = cipher.encrypt_and_digest(data)
+
+file_out = open("temp/encrypted.bin", "wb")
+[ file_out.write(x) for x in (cipher.nonce, tag, ciphertext) ]
+file_out.close()
