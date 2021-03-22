7d5982dc8fa320b2eb3359eb206064da0ba23bec
chaosannals <chaosannals@outlook.com>
2021-03-22 11:48:36 +0800

    diff

--- main/list.py
+++ main/list.py
@@ -10,3 +10,11 @@ set_v(a, 2, 3)
 set_v(a, 0, 2)
 
 print(a)
+
+aset = set(['123', '456', '789'])
+bset = set(['456', '789', '012'])
+adb = list(aset - bset)
+bda = list(bset - aset)
+print(adb)
+print(bda)
+print(len(aset))
