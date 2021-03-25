397e4f7deee5d89a6fd079e0071ebbd8186b66aa
chaosannals <chaosannals@outlook.com>
2021-03-25 13:42:05 +0800

    finally exception

---
+++ main/fny.py
@@ -0,0 +1,12 @@
+import time
+
+try:
+    for i in range(10):
+        time.sleep(2.0)
+        print(f'{i} times sleep 2s')
+except KeyboardInterrupt as e:
+    print(f'k {e}')
+else: # 没有异常则继续的代码
+    print('unknow')
+finally:
+    print('finally')
