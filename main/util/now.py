7bcefa3ad84a99dd984e46c59507273ce56deaa3
chaosannals <chaosannals@outlook.com>
2021-05-27 11:00:18 +0800

    date

--- main/util/now.py
+++ main/util/now.py
@@ -1,5 +1,15 @@
 import datetime
 
 n = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+n11 = datetime.datetime.strptime(n, '%Y-%m-%d %H:%M:%S')
 
 print(n)
+print(n11)
+
+n2 = datetime.date.today().strftime('%Y-%m-%d')
+n22 = datetime.datetime.strptime(n2, '%Y-%m-%d').date()
+n33 = datetime.datetime.strptime('2020-02-01', '%Y-%m-%d').date()
+
+print(n2)
+print((n22 - n33).days)
+print(n22 + datetime.timedelta(days=2))
