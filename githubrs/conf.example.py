2a6f1ac55f60e0f6aa7bb4f6745208fa17c301c7
chaosannals <chaosannals@outlook.com>
2023-06-05 14:06:17 +0800

    save

--- githubrs/conf.example.py
+++ githubrs/conf.example.py
@@ -1,5 +1,7 @@
 CONF = {
     'user': 'chaosannals',
     # 'proxies': {'https://': 'http://127.0.0.1:1088'},
+    # 需要 pip install requests[socks] 获取带 pysocks 的依赖
+    # 'proxies': {'https': 'socks5h://127.0.0.1:1080'},
     'target': 'G:\\',
 }
