91d51b32ec2933a36356359c0a06f20459e2d0a0
chaosannals <chaosannals@outlook.com>
2023-10-07 09:38:39 +0800

    save

---
+++ codemo/qrcode_segno_demo.py
@@ -0,0 +1,37 @@
+# pip install segno
+# pip install pillow
+# pip install qrcode-artistic
+
+import os
+import segno
+
+# 简单
+qrcode = segno.make('Yellow Submarine')
+qrcode.save('simple.png', scale=10)
+
+# 色彩
+qrcode.save(
+    'color.png',
+    dark="red",
+    light="#4499ff",
+    scale=10,
+    border=10,
+)
+
+qrcode.save(
+    'color2.png',
+    dark="red",
+    light="#4499ff",
+    scale=10,
+    border=10,
+    finder_dark="yellow"
+)
+
+# gif 图
+here = os.path.dirname(__file__)
+
+qrcode.to_artistic(
+    background=os.path.join(here, 'bg.gif'),
+    target='bg.out.gif',
+    scale=8,
+)
