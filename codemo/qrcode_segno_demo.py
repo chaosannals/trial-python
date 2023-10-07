bd36bca4f788e2706417c4d95646fee2d90494ab
chaosannals <chaosannals@outlook.com>
2023-10-07 09:52:38 +0800

    save

--- codemo/qrcode_segno_demo.py
+++ codemo/qrcode_segno_demo.py
@@ -27,7 +27,7 @@ qrcode.save(
     finder_dark="yellow"
 )
 
-# gif 图
+# 使用 qrcode-artistic 库生成 gif 图
 here = os.path.dirname(__file__)
 
 # to_artistic 由 qrcode-artistic 库扩展提供。
@@ -36,3 +36,17 @@ qrcode.to_artistic(
     target='bg.out.gif',
     scale=8,
 )
+
+# PIL 处理图片
+
+'''
+二维码中心图片：
+1. 四个边角没有被遮挡
+2. 遮挡面积不超过三分之一
+就可以通过容错矫正。
+所以中心图片是直接遮挡在上面的。
+
+二维码彩图：
+1. 满足二维码规范
+2. 前景色比背景色深即可（彩图在识别时应该是直接被转成灰色图了）。
+'''
